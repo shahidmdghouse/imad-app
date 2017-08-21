051984b7baa9a134f635b5606136b29448b8afd6
shahidmdghouse <30998478+shahidmdghouse@users.noreply.github.com>
2017-08-21 15:37:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/artical-one', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
+app.get('/article-one', function (req, res){
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/artical-two', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'artical-two.html'));
+app.get('/article-two', function (req, res){
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/artical-three', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'artical-three.html')); 
+app.get('/article-three', function (req, res){
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
