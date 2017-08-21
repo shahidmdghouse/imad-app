b14f1faa8d930b4bd4d5a7f7efd68b3d284e4b2d
shahidmdghouse <30998478+shahidmdghouse@users.noreply.github.com>
2017-08-21 19:53:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+
+
+//change the text on main-text div
+
+var element = document.getElementById('main-text');
+
+element.innerHTML = 'this is changed text, a change made by javascript.';
