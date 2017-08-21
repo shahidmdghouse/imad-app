9af9cbb32a265cbf42c31ad8954c5f0ba1a83f37
shahidmdghouse <30998478+shahidmdghouse@users.noreply.github.com>
2017-08-21 18:41:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
+var articles = {
+    'article-one' : {
     title: 'Article One | Shahid Ghouse',
     heading: 'Article One',
     date: 'Aug 8 2016',
@@ -20,6 +21,24 @@ var articleOne = {
               <p>
                   This a Article One page Response, This a Article One page Response, This a Article One page Response, This a Article One page Response, This a Article One page Response, This a Article One page Response, This a Article One page Response,
               </p>`
+},
+    'article-two' : {
+        title: 'Article Two | Shahid Ghouse',
+        heading: 'Article Two',
+        date: 'Sep 8 2016',
+        content: `<p>
+                      This a Article Two page Response.
+                  </p>`
+    },
+    'article-three' : {
+        title: 'Article Three | Shahid Ghouse',
+        heading: 'Article Three',
+        date: 'Sep 8 2016',
+        content: `<p>
+                      This a Article Three page Response.
+                  </p>`
+    }
+
 };
 
 function createTemplete (data){
@@ -59,23 +78,23 @@ function createTemplete (data){
 return htmlTemplete;
 }
 
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res){
-   res.send(createTemplete(articleOne));
+app.get('/:articleName', function (req, res){
+   //res.send(createTemplete(articleOne));
+   var articleName = req.params.articleName;
+   res.send(createTemplete(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res){
+/*app.get('/article-two', function (req, res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res){
    res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
-});
+});*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
