750c370fe494c5b06d30f5f8f847d6003d00409e
shahidmdghouse <30998478+shahidmdghouse@users.noreply.github.com>
2017-08-21 20:12:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,11 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'this is changed text, a change made by javascript.';
+
+//move the image
+
+var img = document.getElementById('madi');
+
+img.onclick = function(){
+ img.style.marginLeft = '100px';   
+};
