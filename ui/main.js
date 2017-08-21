97822d9d76ccfe036ae8a81916aadddd7d248beb
shahidmdghouse <30998478+shahidmdghouse@users.noreply.github.com>
2017-08-21 20:24:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,13 @@ var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px' ; 
+    
+    if(marginLeft > 50){
+        marginLeft = 0;
+        //img.style.marginRight = marginLeft + 'px' ;
+    }
 }
+
 
 img.onclick = function(){
  var interval = setInterval(moveRight, 50);
